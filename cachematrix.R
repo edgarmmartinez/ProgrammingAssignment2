7a344fd4d7dd306882f5af2a0af02d208b8ef5a3
Edgar Martinez <edgarmmartinez@gmail.com>
2015-02-16 16:37:22 -0600

    Programming assignment #2 for week three of R-Programming course

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,64 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions to create and cache a matrix inverse
 
-## Write a short comment describing this function
+## Create matrix object that can cache its invers
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    ## Local variables
+    myMatrix <- x
+    myInvMatrix <- NULL
+    
+    # set raw/original matrix
+    setMatrix <- function(m) {
+        myMatrix <<- m
+    }
+    
+    #get raw/original matrix
+    getMatrix <- function() {
+        myMatrix
+    }
+    
+    #set inverse of original matrix
+    setInv <- function(y) {
+        myInvMatrix <<- y
+    }
+    
+    #get cached inverse of original matrix
+    getInv <- function() {
+        myInvMatrix
+    }
+    
+    # return list of getters and setters
+    list(setMatrix = setMatrix,
+         getMatrix = getMatrix,
+         setInv = setInv,
+         getInv = getInv)
 }
 
 
-## Write a short comment describing this function
-
+## Return a matrix that is the inverse of 'x'. 'x' is an object
+## created by makeCacheMatrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    #try to get the cached matrix inverse
+    m<-x$getInv()
+    
+    if(is.null(m)) {
+        #create the matrix inverse if it's never been done before
+        print("Creating cache") # for illustration purposes
+        m<-solve(x$getMatrix())
+        
+        #store the newly created matrix inverse
+        x$setInv(m)
+    }
+    
+    #return the matrix inverse, whether it came from cache or just created
+    m
 }
+
+#m1<-makeCacheMatrix(matrix(round(rnorm(100,mean=10,sd=3)),10,10))
+#m2<-makeCacheMatrix(matrix(round(rnorm(100,mean=10,sd=3)),10,10))
+#m3<-makeCacheMatrix(matrix(round(runif(100,1,10)),10,10))
+#m4<-makeCacheMatrix(matrix(round(runif(100,10,100)),10,10))
+#cacheSolve(m1)
+#cacheSolve(m2)
+#cacheSolve(m3)
+#cacheSolve(m4)
